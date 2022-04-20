b1d7381d982d13e15620e01502bdf22db3ae660a
Kevinzm15 <100393012+Kevinzm15@users.noreply.github.com>
2022-04-20 15:42:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
 function CasualYellowShirt() {
-  document.getElementById('shirt').src='FitX/casual yellow.jpg';
+  document.getElementById('shirt').src='docs/casual yellow.jpg';
   document.getElementById('prices').innerHTML='$11.99';
 }
 function CasualWhiteShirt() {
-  document.getElementById('shirt').src='FitX/white shirt.jpg';
+  document.getElementById('shirt').src='docs/white shirt.jpg';
   document.getElementById('prices').innerHTML='$9.99';
 }
 function CasualBlackShirt() {
-  document.getElementById('shirt').src='FitX/casual black.jpg';
+  document.getElementById('shirt').src='docs/casual black.jpg';
   document.getElementById('prices').innerHTML='$10.99';
 }
 function FittedBlueShirt() {
-  document.getElementById('shirt').src='FitX/blue fitted.jpg';
+  document.getElementById('shirt').src='docs/blue fitted.jpg';
   document.getElementById('prices').innerHTML='$14.99';
 }
 function FittedGrayShirt() {
-  document.getElementById('shirt').src='FitX/gray fitted.jpg';
+  document.getElementById('shirt').src='docs/gray fitted.jpg';
   document.getElementById('prices').innerHTML='$13.99';
 }
 function FittedGreenShirt() {
-  document.getElementById('shirt').src='FitX.gitbhub.io/green fitted.jpg';
+  document.getElementById('shirt').src='docs.gitbhub.io/green fitted.jpg';
   document.getElementById('prices').innerHTML='$13.99';
 }
 function FittedRedShirt() {
-  document.getElementById('shirt').src='FitX/red fitted.jpg';
+  document.getElementById('shirt').src='docs/red fitted.jpg';
   document.getElementById('prices').innerHTML='$14.99';
 }
 function FittedWhiteShirt() {
-  document.getElementById('shirt').src='FitX/fitted white.jpg';
+  document.getElementById('shirt').src='docs/fitted white.jpg';
   document.getElementById('prices').innerHTML='$13.99';
 }
 function BlackSleeveless() {
-  document.getElementById('shirt').src='FitX/sleeveless hoodie black.jpg';
+  document.getElementById('shirt').src='docs/sleeveless hoodie black.jpg';
   document.getElementById('prices').innerHTML='$17.99';
 }
 function RedSleeveless() {
-  document.getElementById('shirt').src='FitX/sleeveless hoodie red.jpg';
+  document.getElementById('shirt').src='docs/sleeveless hoodie red.jpg';
   document.getElementById('prices').innerHTML='$19.99';
 }
 function GraySleeveless() {
-  document.getElementById('shirt').src='FitX/sleeveless hoodie gray.jpg';
+  document.getElementById('shirt').src='docs/sleeveless hoodie gray.jpg';
   document.getElementById('prices').innerHTML='$18.99';
 }
 function BlueSleeveless() {
-  document.getElementById('shirt').src='FitX/sleeveless hoodie blue.jpg';
+  document.getElementById('shirt').src='docs/sleeveless hoodie blue.jpg';
   document.getElementById('prices').innerHTML='$19.99';
 }
 function BlackHoodie() {
-  document.getElementById('shirt').src='FitX/hoodie black.jpg';
+  document.getElementById('shirt').src='docs/hoodie black.jpg';
   document.getElementById('prices').innerHTML='$25.99';
 }
 function BlackWhiteHoodie() {
-  document.getElementById('shirt').src='FitX/hoodie blackwhite.jpg';
+  document.getElementById('shirt').src='docs/hoodie blackwhite.jpg';
   document.getElementById('prices').innerHTML='$25.99';
 }
 function RedHoodie() {
-  document.getElementById('shirt').src='FitX/hoodie red.jpg';
+  document.getElementById('shirt').src='docs/hoodie red.jpg';
   document.getElementById('prices').innerHTML='$27.99';
 }
 function BlueHoodie() {
-  document.getElementById('shirt').src='FitX/hoodie blue.jpg';
+  document.getElementById('shirt').src='docs/hoodie blue.jpg';
   document.getElementById('prices').innerHTML='$27.99';
 }
 function LightHoodie() {
-  document.getElementById('shirt').src='FitX/hoodie light.jpg';
+  document.getElementById('shirt').src='docs/hoodie light.jpg';
   document.getElementById('prices').innerHTML='$27.99';
 }
