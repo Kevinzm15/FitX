2a9fb04fd77a3d68fe6735ab7f4b2df52ef12443
Kevinzm15 <100393012+Kevinzm15@users.noreply.github.com>
2022-04-20 13:50:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
 function CasualYellowShirt() {
-  document.getElementById('shirt').src='shirts/casual yellow.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/casual yellow.jpg';
   document.getElementById('prices').innerHTML='$12.99';
 }
 function CasualWhiteShirt() {
-  document.getElementById('shirt').src='shirts/white shirt.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/white shirt.jpg';
   document.getElementById('prices').innerHTML='$9.99';
 }
 function CasualBlackShirt() {
-  document.getElementById('shirt').src='shirts/casual black.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/casual black.jpg';
   document.getElementById('prices').innerHTML='$12.99';
 }
 function FittedBlueShirt() {
-  document.getElementById('shirt').src='shirts/blue fitted.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/blue fitted.jpg';
   document.getElementById('prices').innerHTML='$14.99';
 }
 function FittedGrayShirt() {
-  document.getElementById('shirt').src='shirts/gray fitted.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/gray fitted.jpg';
   document.getElementById('prices').innerHTML='$13.99';
 }
 function FittedGreenShirt() {
-  document.getElementById('shirt').src='shirts/green fitted.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/green fitted.jpg';
   document.getElementById('prices').innerHTML='$13.99';
 }
 function FittedRedShirt() {
-  document.getElementById('shirt').src='shirts/red fitted.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/red fitted.jpg';
   document.getElementById('prices').innerHTML='$14.99';
 }
 function FittedWhiteShirt() {
-  document.getElementById('shirt').src='shirts/fitted white.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/fitted white.jpg';
   document.getElementById('prices').innerHTML='$13.99';
 }
 function BlackSleeveless() {
-  document.getElementById('shirt').src='shirts/sleeveless hoodie black.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/sleeveless hoodie black.jpg';
   document.getElementById('prices').innerHTML='$17.99';
 }
 function RedSleeveless() {
-  document.getElementById('shirt').src='shirts/sleeveless hoodie red.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/sleeveless hoodie red.jpg';
   document.getElementById('prices').innerHTML='$19.99';
 }
 function GraySleeveless() {
-  document.getElementById('shirt').src='shirts/sleeveless hoodie gray.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/sleeveless hoodie gray.jpg';
   document.getElementById('prices').innerHTML='$18.99';
 }
 function BlueSleeveless() {
-  document.getElementById('shirt').src='shirts/sleeveless hoodie blue.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/sleeveless hoodie blue.jpg';
   document.getElementById('prices').innerHTML='$19.99';
 }
 function BlackHoodie() {
-  document.getElementById('shirt').src='shirts/hoodie black.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/hoodie black.jpg';
   document.getElementById('prices').innerHTML='$25.99';
 }
 function BlackWhiteHoodie() {
-  document.getElementById('shirt').src='shirts/hoodie blackwhite.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/hoodie blackwhite.jpg';
   document.getElementById('prices').innerHTML='$25.99';
 }
 function RedHoodie() {
-  document.getElementById('shirt').src='shirts/hoodie red.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/hoodie red.jpg';
   document.getElementById('prices').innerHTML='$27.99';
 }
 function BlueHoodie() {
-  document.getElementById('shirt').src='shirts/hoodie blue.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/hoodie blue.jpg';
   document.getElementById('prices').innerHTML='$27.99';
 }
 function LightHoodie() {
-  document.getElementById('shirt').src='shirts/hoodie light.jpg';
+  document.getElementById('shirt').src='FitX.gitbhub/hoodie light.jpg';
   document.getElementById('prices').innerHTML='$27.99';
 }
